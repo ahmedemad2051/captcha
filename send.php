402a0a40ba8bff441b119a8ef113f60be362e209
ahmedemad2051 <ahmed.emad2051@gmail.com>
2016-10-07 11:17:08 +0200

    upload

---
+++ send.php
@@ -0,0 +1,22 @@
+<?php
+
+
+if(isset($_POST['send']))
+{
+	$con = new PDO('mysql:host=localhost;dbname=captcha','root','root');
+	$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+	$name=$_POST['name'];
+	echo $name;
+	try{
+		$query='INSERT INTO contact  (name) VALUE ("'.$name.'")';
+		$result=$con->query($query);
+		if($result)
+		{
+			header('Location:index.html');
+		}
+	}
+	catch(Exception $e){
+		echo $e->getMessage();
+	}
+	
+}
